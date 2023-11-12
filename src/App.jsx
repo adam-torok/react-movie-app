097c375790bbc0d98889e91f0c612a9694bbb1b3
Török Ádám <adam-torok@outlook.hu>
2023-11-12 21:35:41 +0100

    Init tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import './App.css'
+import { useEffect } from "react";
+import Header from './components/nav/Header'
+import './app.css';
 
-function App() {
-  return (<h1>Hello world</h1>)
+const App = () => {
+    const API_URL = import.meta.env.VITE_MOVIE_APP_URL;
+    const API_KEY = import.meta.env.VITE_MOVIE_API_KEY;
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}?apikey=${API_KEY}&s=${title}`);
+        const data = await response.json();
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    });
+
+    return (
+        <>
+            <Header />
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </>
+    );
 }
 
-export default App
+export default App;
